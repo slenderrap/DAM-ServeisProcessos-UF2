d40f027e4a677afb4b2ed1d110b8772cde6f2e35
optimisme <optimisme@gmail.com>
2025-03-06 11:35:59 +0100

    .

--- 07 NodeJS - WebSockets/EinaJocs/eina_jocs/linux/flutter/generated_plugins.cmake
+++ 07 NodeJS - WebSockets/EinaJocs/eina_jocs/linux/flutter/generated_plugins.cmake
@@ -4,8 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_window
-  screen_retriever_linux
-  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- 07 NodeJS - WebSockets/EinaJocs/eina_jocs/windows/flutter/generated_plugins.cmake
+++ 07 NodeJS - WebSockets/EinaJocs/eina_jocs/windows/flutter/generated_plugins.cmake
@@ -4,8 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_window
-  screen_retriever_windows
-  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
