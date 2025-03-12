f55b28321b7b89b2e08a61e68f7a275605fad09c
optimisme <optimisme@gmail.com>
2025-03-12 17:27:26 +0100

    .

--- 07 NodeJS - WebSockets/EinaJocs/eina_jocs/lib/app_data.dart
+++ 07 NodeJS - WebSockets/EinaJocs/eina_jocs/lib/app_data.dart
@@ -126,7 +126,7 @@ class AppData extends ChangeNotifier {
 
   Future<ui.Image> getImage(String imageFileName) async {
     if (!imagesCache.containsKey(imageFileName)) {
-      final File file = File(imageFileName);
+      final File file = File("$filePath/$imageFileName");
       if (!await file.exists()) {
         throw Exception("File does not exist: $imageFileName");
       }
